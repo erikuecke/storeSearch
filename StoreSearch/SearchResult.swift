78e2b871f7c08e3b700be9a33af6a8db798aadb7
Erik Uecke <erikuecke@gmail.com>
2017-09-22 17:44:59 -0400

    Add tableview and searchbar to the app

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Erik Uecke on 9/22/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+    
+}
