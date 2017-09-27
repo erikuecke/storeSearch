7c757a8b78d5526129931efdd814e220da819624
Erik Uecke <erikuecke@gmail.com>
2017-09-27 15:23:52 -0400

    Add URLSession method to network request.

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -69,22 +69,43 @@ extension SearchViewController: UISearchBarDelegate {
             searchResults = []
             
             // 1
-            let queue = DispatchQueue.global()
-            let url = self.iTunesURL(searchText: searchBar.text!)
+            let url = iTunesURL(searchText: searchBar.text!)
             // 2
-            queue.async {
+            let session = URLSession.shared
+            // 3
+            let dataTask = session.dataTask(with: url, completionHandler: { data, response, error in
                 
-                if let data = self.performStorRequest(with: url) {
-                    self.searchResults = self.parse(data: data)
-                    self.searchResults.sort(by: <)
-                    // 3
-                    DispatchQueue.main.async {
-                        self.isLoading = false
-                        self.tableView.reloadData()
+//                print("On main thread? " + (Thread.current.isMainThread ? "Yes" : "No"))
+
+                // 4
+                if let error = error {
+                    print("Failure! \(error)")
+                    
+                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    if let data = data {
+                        self.searchResults = self.parse(data: data)
+                        self.searchResults.sort(by: <)
+                        DispatchQueue.main.async {
+                            self.isLoading = false
+                            self.tableView.reloadData()
+                        }
+                        return
                     }
-                    return
+                    
+                } else {
+                    print("Failure! \(response!)")
                 }
-            }
+                
+                DispatchQueue.main.async {
+                    self.hasSearched = false
+                    self.isLoading = false
+                    self.tableView.reloadData()
+                    self.showNetworkError()
+                }
+                
+            })
+            // 5
+            dataTask.resume()
         }
     
     }
@@ -157,16 +178,6 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         
     }
     
-    // Perform the search request method
-    func performStorRequest(with url: URL) -> Data? {
-        do {
-            return try Data(contentsOf: url)
-        } catch {
-            print("Download Error; \(error.localizedDescription)")
-            showNetworkError()
-            return nil
-        }
-    }
 
     // Parse Method
     func parse(data: Data) -> [SearchResult] {
